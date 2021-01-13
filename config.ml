e7ce5a0ab64e6e3b22c4abbf02fcb47331b52b3a
dinosaure <romain.calascibetta@gmail.com>
2021-01-13 20:49:04 +0100

    Upgrade the unikernel to git.3.3.0

--- config.ml
+++ config.ml
@@ -1,32 +1,158 @@
-(* (c) 2017, 2018 Hannes Mehnert, all rights reserved *)
-
 open Mirage
+
+type mimic = Mimic
+
+let mimic = typ Mimic
+
+let mimic_count =
+  let v = ref (-1) in
+  fun () -> incr v ; !v
+
+let mimic_conf () =
+  let packages = [ package "mimic" ] in
+  impl @@ object
+       inherit base_configurable
+       method ty = mimic @-> mimic @-> mimic
+       method module_name = "Mimic.Merge"
+       method! packages = Key.pure packages
+       method name = Fmt.str "merge_ctx%02d" (mimic_count ())
+       method! connect _ _modname =
+         function
+         | [ a; b ] -> Fmt.str "Lwt.return (Mimic.merge %s %s)" a b
+         | [ x ] -> Fmt.str "%s.ctx" x
+         | _ -> Fmt.str "Lwt.return Mimic.empty"
+     end
+
+let merge ctx0 ctx1 = mimic_conf () $ ctx0 $ ctx1
+
+let mimic_tcp_conf =
+  let packages = [ package "git-mirage" ~sublibs:[ "tcp" ] ] in
+  impl @@ object
+       inherit base_configurable
+       method ty = stackv4 @-> mimic
+       method module_name = "Git_mirage_tcp.Make"
+       method! packages = Key.pure packages
+       method name = "tcp_ctx"
+       method! connect _ modname = function
+         | [ stack ] ->
+           Fmt.str {ocaml|Lwt.return (%s.with_stack %s %s.ctx)|ocaml}
+             modname stack modname
+         | _ -> assert false
+     end
+
+let mimic_tcp_impl stackv4 = mimic_tcp_conf $ stackv4
+
+let mimic_ssh_conf ~kind ~seed ~auth =
+  let seed = Key.abstract seed in
+  let auth = Key.abstract auth in
+  let packages = [ package "git-mirage" ~sublibs:[ "ssh" ] ] in
+  impl @@ object
+       inherit base_configurable
+       method ty = stackv4 @-> mimic @-> mclock @-> mimic
+       method! keys = [ seed; auth; ]
+       method module_name = "Git_mirage_ssh.Make"
+       method! packages = Key.pure packages
+       method name = match kind with
+         | `Rsa -> "ssh_rsa_ctx"
+         | `Ed25519 -> "ssh_ed25519_ctx"
+       method! connect _ modname =
+         function
+         | [ _; tcp_ctx; _ ] ->
+             let with_key =
+               match kind with
+               | `Rsa -> "with_rsa_key"
+               | `Ed25519 -> "with_ed25519_key"
+             in
+             Fmt.str
+               {ocaml|let ssh_ctx00 = Mimic.merge %s %s.ctx in
+                      let ssh_ctx01 = Option.fold ~none:ssh_ctx00 ~some:(fun v -> %s.%s v ssh_ctx00) %a in
+                      let ssh_ctx02 = Option.fold ~none:ssh_ctx01 ~some:(fun v -> %s.with_authenticator v ssh_ctx01) %a in
+                      Lwt.return ssh_ctx02|ocaml}
+               tcp_ctx modname
+               modname with_key Key.serialize_call seed
+               modname Key.serialize_call auth
+         | _ -> assert false
+     end
+
+let mimic_ssh_impl ~kind ~seed ~auth stackv4 mimic_git mclock =
+  mimic_ssh_conf ~kind ~seed ~auth
+  $ stackv4
+  $ mimic_git
+  $ mclock
+
+(* TODO(dinosaure): user-defined nameserver and port. *)
+
+let mimic_dns_conf =
+  let packages = [ package "git-mirage" ~sublibs:[ "dns" ] ] in
+  impl @@ object
+       inherit base_configurable
+       method ty = random @-> mclock @-> time @-> stackv4 @-> mimic @-> mimic
+       method module_name = "Git_mirage_dns.Make"
+       method! packages = Key.pure packages
+       method name = "dns_ctx"
+       method! connect _ modname =
+         function
+         | [ _; _; _; stack; tcp_ctx ] ->
+             Fmt.str
+               {ocaml|let dns_ctx00 = Mimic.merge %s %s.ctx in
+                      let dns_ctx01 = %s.with_dns %s dns_ctx00 in
+                      Lwt.return dns_ctx01|ocaml}
+               tcp_ctx modname
+               modname stack
+         | _ -> assert false
+     end
+
+let mimic_dns_impl random mclock time stackv4 mimic_tcp =
+  mimic_dns_conf $ random $ mclock $ time $ stackv4 $ mimic_tcp
+
+(* / *)
 
 let remote_k =
   let doc = Key.Arg.info ~doc:"Remote git repository." ["r"; "remote"] in
-  Key.(create "remote" Arg.(opt string "https://github.com/roburio/udns.git" doc))
+  Key.(create "remote" Arg.(opt string "git@localhost:mirage/zone" doc))
 
 let axfr =
   let doc = Key.Arg.info ~doc:"Allow unauthenticated zone transfer." ["axfr"] in
   Key.(create "axfr" Arg.(flag doc))
+
+let ssh_seed =
+  let doc = Key.Arg.info ~doc:"Seed of the private SSH key." [ "ssh-seed" ] in
+  Key.(create "ssh_seed" Arg.(opt (some string) None doc))
+
+let ssh_auth =
+  let doc = Key.Arg.info ~doc:"SSH public key of the remote Git endpoint." [ "ssh-auth" ] in
+  Key.(create "ssh_auth" Arg.(opt (some string) None doc))
 
 let dns_handler =
   let packages = [
     package "logs" ;
     package ~min:"4.3.0" ~sublibs:["mirage"; "zone"] "dns-server";
     package "dns-tsig";
-    package ~min:"2.0.0" "irmin-mirage";
-    package ~min:"2.0.0" "irmin-mirage-git";
+    package ~min:"2.5.1" "irmin-mirage";
+    package ~min:"2.5.1" "irmin-mirage-git";
+    package ~min:"3.3.0" "git";
     package "conduit-mirage";
   ] in
   foreign
     ~keys:[Key.abstract remote_k ; Key.abstract axfr]
     ~packages
     "Unikernel.Main"
-    (random @-> pclock @-> mclock @-> time @-> stackv4 @-> resolver @-> conduit @-> job)
+    (random @-> pclock @-> mclock @-> time @-> stackv4 @-> resolver @-> conduit @-> mimic @-> job)
+
+let mimic ~kind ~seed ~auth stackv4 random mclock time =
+  let mtcp = mimic_tcp_impl stackv4 in
+  let mdns = mimic_dns_impl random mclock time stackv4 mtcp in
+  let mssh = mimic_ssh_impl ~kind ~seed ~auth stackv4 mtcp mclock in
+  merge mssh mdns
 
 let () =
   let net = generic_stackv4 default_network in
+  let random = default_random in
+  let pclock = default_posix_clock in
+  let mclock = default_monotonic_clock in
+  let time = default_time in
+  let conduit = conduit_direct ~tls:true net in
+  let resolver = resolver_dns ~random ~time ~mclock net in
+  let mimic = mimic ~kind:`Rsa ~seed:ssh_seed ~auth:ssh_auth net random mclock time in
   register "primary-git"
-    [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $
-     default_time $ net $ resolver_dns net $ conduit_direct ~tls:true net]
+    [ dns_handler $ random $ pclock $ mclock $ time $ net $ resolver $ conduit $ mimic ]
