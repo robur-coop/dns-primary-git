ad31087028ed18bf34b44fb370d56c7eb8cbf104
Hannes Mehnert <hannes@mehnert.org>
2022-04-08 16:33:56 +0200

    update to dns 6.2.2, also allow other subdirectories in git repository

--- config.ml
+++ config.ml
@@ -191,7 +191,7 @@ let mimic_impl =
 let dns_handler =
   let packages = [
     package "logs" ;
-    package ~min:"5.0.0" ~sublibs:["mirage"; "zone"] "dns-server";
+    package ~min:"6.2.2" ~sublibs:["mirage"; "zone"] "dns-server";
     package "dns-tsig";
     package ~min:"2.10.0" ~max:"3.0.0" "irmin-mirage";
     package ~min:"2.10.0" "irmin-mirage-git";
